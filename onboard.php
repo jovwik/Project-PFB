a864be752454e014c8772f0b33f75b511b302c7d
filbert288 <filbert.tedja6@gmail.com>
2025-12-13 15:22:56 +0700

    Add files via upload

---
+++ onboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <img src="assets/PAWHUB.png" alt="Logo">
+        <a href="Home.php">Home</a>
+    </nav>
+
+    <section class="container">
+    <div class="card">
+        <img src="assets/PAWHUB.png" alt="BiteJoe" class="card-logo">
+        <p>Discover Your Healthy Choice</p>
+
+        <div id="opt">
+            <a href="register.php" class="signup">SIGN UP</a>
+            <a href="login.php" class="login">LOG IN</a>
+        </div>
+
+
+    </div>
+    </section>
+
+</body>
+</html>
